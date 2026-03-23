43cfef6062a0f0cff3f89c01c3f74a3086d520fc
roooot <103732419+rooootdev@users.noreply.github.com>
2026-03-23 21:52:45 +0100

    Initial Commit

---
+++ lara/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lara
+//
+//  Created by ruter on 23.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lara/laraApp.swift
@@ -0,0 +1,17 @@
+//
+//  laraApp.swift
+//  lara
+//
+//  Created by ruter on 23.03.26.
+//
+
+import SwiftUI
+
+@main
+struct laraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
